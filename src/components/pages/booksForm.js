219d9ab45f5e2a16263f930d185733c40004a3cd
ezra sandzer-bell <equilibriumarts@gmail.com>
2017-08-16 17:08:20 -0700

    Creat form input with bootstrap styling

---
+++ src/components/pages/booksForm.js
@@ -0,0 +1,42 @@
+"use strict"
+
+import React from 'react';
+import {Well, Panel, FormControl, FormGroup, ControlLabel, Button} from 'react-bootstrap';
+
+class BooksForm extends React.Component {
+  render() {
+    return(
+      <Well>
+        <Panel>
+          <FormGroup controlId="title">
+            <ControlLabel>Title</ControlLabel>
+            <FormControl
+              type="text"
+              placeholder="Enter Title"
+              ref="title"
+            />
+          </FormGroup>
+          <FormGroup controlId="description">
+            <ControlLabel>Title</ControlLabel>
+            <FormControl
+              type="text"
+              placeholder="Enter Description"
+              ref="description"
+            />
+          </FormGroup>
+          <FormGroup controlId="price">
+            <ControlLabel>Title</ControlLabel>
+            <FormControl
+              type="text"
+              placeholder="Enter Price"
+              ref="price"
+            />
+          </FormGroup>
+          <Button bsStyle='primary'>Save Book</Button>
+        </Panel>
+      </Well>
+    )
+  }
+}
+
+export default BooksForm;
